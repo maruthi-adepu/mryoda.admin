e1121a5048fd346a75bdfd7a3e8c76335d715f46
maruthi-adepu <maruthi.adepu@tekreant.com>
2024-10-30 14:15:44 +0530

    Dashboard Repository setUp

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='flex justify-center items-center'>
+      <Button variant="contained">Login</Button>
+
+    </div>
+  )
+}
+
+export default Login
---
+++ src/app/viewmember/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      View member
+    </div>
+  )
+}
+
+export default page

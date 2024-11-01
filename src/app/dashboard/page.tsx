66094074d3a3f79fccf5d652c5e440eda4a02fbb
maruthi-adepu <maruthi.adepu@tekreant.com>
2024-11-01 10:55:50 +0530

    TopBar UI Development

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,6 +6,7 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import TopBar from '@/components/topbar';
 
 const cardData = [
     {
@@ -33,21 +34,23 @@ const page = () => {
 
     return (
         <>
+        <TopBar/>
             <section className='grid gap-[16px] bg-[#FFFFFF]'>
 
                 <Container maxWidth="xl">
                     <Typography
                         variant="h6"
-                        style={{ fontWeight: 700, color: '#000000CC', fontSize: '20px', marginLeft:"35px" }}
+                        sx={{ fontWeight: 700, color: '#000000CC', fontSize: '20px', paddingY:2 }}
+
                     >
                         Key Statistics
                     </Typography>
 
-                </Container>
-                <Grid container spacing={4.5} sx={{ padding: 1, justifyContent: 'center' }}>
+
+                    <Grid container spacing={2} sx={{  justifyContent: 'center' }}>
                     {cardData.map((card, index) => (
-                        <Grid item xs={12} sm={6} md={3} key={index} sx={{ padding: '0 8px' }}>
-                            <Card sx={{ maxWidth: 434, maxHeight: 148, backgroundColor: card.color, boxShadow: 'none', border: "#FFFFFF", borderRadius: "8px" }}>
+                        <Grid item xs={12} sm={6} md={3} lg={4} key={index} sx={{ padding: '0 8px', paddingLeft:0 }}>
+                            <Card sx={{ maxWidth: 434, maxHeight: 148, backgroundColor: card.color, boxShadow: 'none', border: "#FFFFFF", borderRadius: "8px", paddingLeft:0, marginBottom:2 }}>
                                 <CardContent>
                                     <Box className="flex justify-between">
                                         <Typography gutterBottom variant="h5" component="div" style={{ fontWeight: 400, color: '#000000CC', fontSize: '17px' }}>
@@ -68,6 +71,8 @@ const page = () => {
                         </Grid>
                     ))}
                 </Grid>
+                </Container>
+              
             </section>
         </>
     )

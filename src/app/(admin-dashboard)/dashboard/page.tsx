85eedaf1127fca1c753a191c2134b41da634d42b
Durga prasad <durgaprasad.sydam@tekreant.com>
2024-11-05 14:34:55 +0530

    dashboard

--- src/app/(admin-dashboard)/dashboard/page.tsx
+++ src/app/(admin-dashboard)/dashboard/page.tsx
@@ -10,9 +10,10 @@ import TopBar from '@/components/topbar';
 import ViewMemberDetails from '@/components/viewMember';
 
 const cardData = [
-    { title: 'Active Members', value: '1,234', percentage: '+5%', color: '#F9F4A0'},
-    { title: 'Expiring Memberships', value: '56', percentage: '+2%', color: '#B0E0E4'},
-    { title: 'Transactions', value: '789', percentage: '+8%', color: '#FCDDE3'},
+    { title: 'Active Memberships', value: '21,484', percentage: '+15%',content:'Compared to last year', color: '#F9F4A0'},
+    { title: 'New Memberships', value: '81', percentage: '+28%',content:'Compared to last month', color: '#B0E0E4'},
+    { title: 'Expired Memberships', value: '764', percentage: '+8%',content:'Attention needed, send remainder', color: '#FCDDE3'},
+    { title: 'Expiring this month', value: '226', percentage: '+8%',content:'Attention needed, send remainder', color: '#c9f5ba'},
 ];
 
 const page = () => {
@@ -31,8 +32,8 @@ const page = () => {
                     </Typography>
                     <Grid container spacing={2} sx={{  justifyContent: 'center' }}>
                     {cardData?.map((card, index) => (
-                        <Grid item xs={12} sm={6} md={3} lg={4} key={index} sx={{ padding: '0 8px', paddingLeft:0 }}>
-                            <Card sx={{ maxWidth: 434, maxHeight: 148, backgroundColor: card.color, boxShadow: 'none', border: "#FFFFFF", borderRadius: "8px", paddingLeft:0, marginBottom:2 }}>
+                        <Grid item xs={12} sm={6} md={3} lg={3} key={index} sx={{ padding: '0 8px', paddingLeft:0 }}>
+                            <Card sx={{backgroundColor: card.color, boxShadow: 'none', border: "#FFFFFF", borderRadius: "8px", paddingLeft:0, marginBottom:2 }}>
                                 <CardContent>
                                     <Box className="flex justify-between">
                                         <Typography gutterBottom variant="h5" component="div" style={{ fontWeight: 400, color: '#000000CC', fontSize: '17px' }}>
@@ -42,13 +43,19 @@ const page = () => {
                                             <MoreHorizIcon />
                                         </Typography>
                                     </Box>
-                                    <Typography variant="body2" sx={{ color: 'text.secondary', marginTop:-2 }} style={{ fontWeight: 700, color: '#000000CC', fontSize: '34px' }}>
+                                    
+                                    <Typography variant="body2" sx={{ color: 'text.secondary'}} style={{ fontWeight: 700, color: '#000000CC', fontSize: '34px' }}>
                                         {card.value}
                                     </Typography>
+                                    
                                 </CardContent>
-                                <CardActions sx={{ marginTop: -3, padding: 0, marginBottom: 1 }}>
+                                <CardActions sx={{ marginTop: -3, padding: 0, marginBottom: 1, display:"flex", flexDirection:"column",alignItems:"self-start" }}>
                                     <Button sx={{ fontWeight: 400, color: '#000000CC', fontSize: '13px', marginLeft: '0px', paddingLeft: "0px" }}>{card.percentage}</Button>
+                                    <Typography variant="caption" sx={{ color: 'text.secondary'}} style={{color: '#000000CC', fontSize: '14px',paddingBottom:"10px",paddingLeft:"4px" }}>
+                                        {card.content}
+                                    </Typography>
                                 </CardActions>
+                                
                             </Card>
                         </Grid>
                     ))}
